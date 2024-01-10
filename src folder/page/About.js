e61b4414e196457c44105dfc32f30dfcda139191
SuhainaFathimaM <153417476+SuhainaFathimaM@users.noreply.github.com>
2024-01-11 01:43:04 +0530

    Create About.js

---
+++ src folder/page/About.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+import { Box, Typography } from "@mui/material";
+
+const About = () => {
+  return (
+    <Layout>
+      <Box
+        sx={{
+          my: 15,
+          textAlign: "center",
+          p: 2,
+          "& h4": {
+            fontWeight: "bold",
+            my: 2,
+            fontSize: "2rem",
+          },
+          "& p": {
+            textAlign: "justify",
+          },
+          "@media (max-width:600px)": {
+            mt: 0,
+            "& h4 ": {
+              fontSize: "1.5rem",
+            },
+          },
+        }}
+      >
+        <Typography variant="h4">Welcome To My Resturant</Typography>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat quod,
+          suscipit, aperiam totam autem culpa cum eveniet dolorum quasi est
+          perspiciatis laborum. Nam recusandae nihil quia odio voluptatibus
+          facere omnis facilis rerum? Ab eum beatae nobis reiciendis, qui
+          temporibus aliquid, nesciunt velit sed quam recusandae necessitatibus,
+          tempora maxime. Repellendu Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
+          deserunt libero reprehenderit cum sint fugit cumque temporibus modi
+        </p>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
+          deserunt libero reprehenderit cum sint fugit cumque temporibus modi
+          facere eveniet amet obcaecati ducimus harum velit maxime vel qui
+          voluptatibus quam odio corrupti saepe, voluptas dolorum quidem
+          tempore? Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
+          deserunt libero reprehenderit cum sint fugit cumque temporibus modi
+        </p>
+      </Box>
+    </Layout>
+  );
+};
+
+export default About;
